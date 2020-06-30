bc73a612f2c9f094db8cc21cb0984a6dc377c2db
hoodaly <hoodaly@gmail.com>
2020-07-01 01:04:03 +0200

    Use local dependencies instead of git tag references in mix file

--- mix.exs
+++ mix.exs
@@ -23,9 +23,12 @@ defmodule Entice.Web.Mixfile do
   defp elixirc_paths(_),     do: ["lib", "web"]
 
   defp deps do
-    [{:entice_logic, github: "entice/logic", ref: "e6dc0a63f52f4301a59201954ee9d06e41636fd5"},
-     {:entice_entity, github: "entice/entity", ref: "c26f6f77ae650e25e6cd2ffea8aae46b7d83966a"},
-     {:entice_utils, github: "entice/utils", ref: "79ead4dca77324b4c24f584468edbaff2029eeab"},
+    [#{:entice_logic, github: "entice/logic", ref: "e3a833c9197edbdb6c43ebffb02a2705ca13bad3"},
+     #{:entice_entity, github: "entice/entity", ref: "c26f6f77ae650e25e6cd2ffea8aae46b7d83966a"},
+     #{:entice_utils, github: "entice/utils", ref: "79ead4dca77324b4c24f584468edbaff2029eeab"},
+     {:entice_logic, path: "../entice_logic"},
+     {:entice_entity, path: "../entice_entity"},
+     {:entice_utils, path: "../entice_utils"},
      {:cowboy, "~> 1.0"},
      {:phoenix, "~> 1.2.0-rc"},
      {:phoenix_pubsub, "~> 1.0.0-rc"},
